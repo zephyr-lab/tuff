d424e50d2359e0bdfd095a01868c0f7e2cf3d6f1
Emin Dursun <83394220+zephyr-lab@users.noreply.github.com>
2025-08-28 09:34:48 +0000

    s

--- ascii.c
+++ ascii.c
@@ -1,1 +1,29 @@
-s
+#include <stdio.h>
+
+#define HEIGHT 10
+#define WIDTH 20   // etwas breiter, damit man die x-Achse besser sieht
+
+int main() {
+    printf("Schritt 1: ASCII Zeichen Test\n");
+    printf("*  #  |  -  +\n\n");
+
+    for (int y = HEIGHT; y >= 0; y--) {   
+        for (int x = 0; x <= WIDTH; x++) { 
+            if (x == 0 && y == 0) {
+                printf("+");   
+            }
+            else if (x == 0) {
+                printf("|");   
+            }
+            else if (y == 0) {
+                printf("-");   
+            }
+            else {
+                printf(" ");   
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
